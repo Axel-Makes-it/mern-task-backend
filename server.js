6c24b83be0a8110f340ff0d6ef649c2cbb0fa4ba
Axel Ivan Santiago <axelivanlux@gmail.com>
2023-11-15 23:07:47 -0500

    update

--- server.js
+++ server.js
@@ -11,11 +11,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "https://mern-task-app-puce.vercel.app"],
-  })
-);
+app.use(cors());
 
 app.use(taskRoutes);
 
