fc6e261c11feb471b9671929e716664193cc5493
Axel Ivan Santiago <axelivanlux@gmail.com>
2023-11-15 23:19:37 -0500

    update

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -9,10 +9,10 @@ const {
   updateTask,
 } = require("../controllers/taskController");
 
-router.post("/api/tasks", createTask);
-router.get("/api/tasks", getTasks);
-router.get("/api/tasks/:id", getTask);
-router.delete("/api/tasks/delete/:id", deleteTask);
-router.put("/api/tasks/update/:id", updateTask);
+router.post("api/tasks", createTask);
+router.get("api/tasks", getTasks);
+router.get("api/tasks/:id", getTask);
+router.delete("api/tasks/delete/:id", deleteTask);
+router.put("api/tasks/update/:id", updateTask);
 
 module.exports = router;
