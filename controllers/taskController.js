0fcc1cf3dbab99133be67bd510343c050beb9216
Axel Ivan Santiago <axelivanlux@gmail.com>
2023-11-15 21:27:19 -0500

    update

---
+++ controllers/taskController.js
@@ -0,0 +1,86 @@
+const Task = require("../models/taskModel");
+
+// Create Task
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+// Get all Tasks
+const getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+// Get a single Task
+const getTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+    if (!task) {
+      return res.status(404).json(`No task with id ${id}`);
+    }
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+// Delete Task
+const deleteTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(`Deleting task with ID: ${id}`);
+    const task = await Task.findByIdAndDelete(id);
+    if (!task) {
+      return res.status(404).json(`No task with id ${id}`);
+    }
+
+    res.status(200).send("Task deleted");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+// Update a Task
+const updateTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Check if the task exists
+    const existingTask = await Task.findById(id);
+    if (!existingTask) {
+      return res.status(404).json(`No task with id ${id}`);
+    }
+
+    // Update the task
+    const updatedTask = await Task.findByIdAndUpdate({ _id: id }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    // if (!task) {
+    //   return res.status(404).json(`No task with id ${id}`);
+    // }
+
+    res.status(200).json(updatedTask);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+module.exports = {
+  createTask,
+  getTasks,
+  getTask,
+  deleteTask,
+  updateTask,
+};
---
+++ server.js
@@ -0,0 +1,41 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/connectDB");
+const mongoose = require("mongoose");
+const Task = require("./models/taskModel");
+const taskRoutes = require("./routes/taskRoute");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://mern-task-app-puce.vercel.app/"],
+  })
+);
+app.use(taskRoutes);
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("<h1> Home Page!</h1>");
+});
+
+// The provided port by the hosting platform or default to 5000
+const port = process.env.PORT || 5000;
+
+// connect database
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
